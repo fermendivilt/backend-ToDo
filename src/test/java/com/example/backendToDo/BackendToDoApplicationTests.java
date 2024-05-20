c6e921060cf9179498974efc64fed99f643aa70e
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-05-20 11:50:54 -0700

    Initial commit
    [Dev] Contains SpringBoot "hello world"

---
+++ src/test/java/com/example/backendToDo/BackendToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
