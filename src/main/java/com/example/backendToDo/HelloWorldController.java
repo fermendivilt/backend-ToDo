a54baab3f3f4c43586a730272a18e483f562dd6a
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-05-21 12:16:06 -0700

    [Dev] Welcome message for "/" GET endpoint

--- src/main/java/com/example/backendToDo/HelloWorldController.java
+++ src/main/java/com/example/backendToDo/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String index(){
-        return "Hello world from Spring boot!";
+        return "Backend is active";
     }
 }
