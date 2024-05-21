f275534a9f729ce01a319fa8a3b870734d50cbe6
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-05-21 12:17:11 -0700

    [Dev] ToDoService injection in its Controller

--- src/main/java/com/example/backendToDo/todo/ToDoController.java
+++ src/main/java/com/example/backendToDo/todo/ToDoController.java
@@ -1,5 +1,22 @@
 package com.example.backendToDo.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ToDoController {
 
+    // qué es el final??
+    private final ToDoService toDoService;
+
+    //Autowired injects dependencies during object creation (this case)
+    //  Also works in setters or directly in fields
+    @Autowired
+    public ToDoController(ToDoService toDoService){
+        this.toDoService = toDoService;
+    }
+
+    
 }
