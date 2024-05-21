41e9a1b80a1fb118ff12ea6ffc35e498dfa6a3fb
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-05-21 10:44:21 -0700

    [Dev] ToDo interface and implementation

--- src/main/java/com/example/backendToDo/todo/ToDoRepository.java
+++ src/main/java/com/example/backendToDo/todo/ToDoRepository.java
@@ -1,5 +1,9 @@
 package com.example.backendToDo.todo;
 
 public interface ToDoRepository {
-    
+    public String GetAll();
+    public ToDo Post();
+    public ToDo PostDone();
+    public ToDo Put();
+    public ToDo PutUndone();
 }
--- src/main/java/com/example/backendToDo/todo/ToDoService.java
+++ src/main/java/com/example/backendToDo/todo/ToDoService.java
@@ -1,5 +1,60 @@
 package com.example.backendToDo.todo;
 
-public class ToDoService implements ToDoRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ToDoService implements ToDoRepository {
+    // We want a singleton for toDoList be the same across the app
+    private static ToDoService instance;
+    private List<ToDo> toDoList = new ArrayList<ToDo>();
+
+    // To prevent direct instantiation
+    private ToDoService() {}
+
+    // Singleton creation or safe obtention
+    public static ToDoService getInstance() {
+        if(instance == null){
+
+            // esto asegura la seguridad de los hilos?
+            synchronized (ToDoService.class){
+
+                if(instance == null){
+                
+                    instance = new ToDoService();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    @Override
+    public String GetAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetAll'");
+    }
+
+    @Override
+    public ToDo Post() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Post'");
+    }
+
+    @Override
+    public ToDo PostDone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'PostDone'");
+    }
+
+    @Override
+    public ToDo Put() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Put'");
+    }
+
+    @Override
+    public ToDo PutUndone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'PutUndone'");
+    }
 }
