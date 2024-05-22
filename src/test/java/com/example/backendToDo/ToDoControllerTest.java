b3851316d08cbf187972b9b55803fe1e76e690c4
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-05-22 12:06:42 -0700

    MockMVC object validation

--- src/test/java/com/example/backendToDo/ToDoControllerTest.java
+++ src/test/java/com/example/backendToDo/ToDoControllerTest.java
@@ -3,6 +3,9 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.example.backendToDo.todo.ToDo;
+import com.example.backendToDo.todo.Priority;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +32,17 @@ public class ToDoControllerTest {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Hello world from Spring boot!")));
+	}
+
+	@Test
+	public void postToDos() throws Exception {
+		ToDo postTest = new ToDo();
+		postTest.name = "Post test";
+		postTest.priority = Priority.Medium;
+
+		MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/todos").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Post test"))
+				.andReturn();
 	}
 }
